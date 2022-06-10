a2c42e1d44699a45a1a35f256873826b8a6d3dc4
biboo <teh.biboo@gmail.com>
2022-06-11 00:04:37 +0200

    Ajout d'un Syso

--- testming/src/main/fr/biboo/Test.java
+++ testming/src/main/fr/biboo/Test.java
@@ -8,7 +8,7 @@ public class Test {
     public static void main(String args[]) {
         // Commentaire
         System.out.println("Hello World !");
-
+        System.out.println("On part sur un nouveau commit !");
     }
 
 }
